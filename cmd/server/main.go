b661e0d37fab27d643aaba73dfc92d4f51d1bacd
John Dudmesh <john@dudmesh.co.uk>
2024-02-21 11:51:31 +0100

    update to latest client

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,128 +1,39 @@
 package main
 
 import (
-	"gohtmx/console"
 	"net/http"
-	"os"
-	"strconv"
 
-	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
-	log "github.com/sirupsen/logrus"
+	client "github.com/jdudmesh/gomon-client"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	log.Info("starting server")
 
-	db, err := sqlx.Connect("sqlite3", "./.gomon/gomon.db")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	t, err := client.NewEcho("./views/*html")
 	if err != nil {
-		log.Fatalf("connecting to sqlite: %v", err)
+		log.Fatal(err)
 	}
+	defer t.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Info("request: ", r.URL.Path)
-
-		runs := []console.LogRun{}
-		err = db.Select(&runs, "SELECT * FROM runs ORDER BY created_at DESC")
+	go func() {
+		err := t.ListenAndServe()
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			log.Error(err)
 		}
-		currentRun := int(runs[0].ID)
+	}()
 
-		run := int(runs[0].ID)
-		runParam := r.URL.Query().Get("run")
-		if !(runParam == "" || runParam == "current") {
-			run, err = strconv.Atoi(runParam)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		}
+	e.Renderer = t
 
-		events := []console.LogEvent{}
-		err = db.Select(&events, "SELECT * FROM events WHERE run_id = ? ORDER BY created_at ASC", run)
-		if err != nil {
-			log.Errorf("getting event: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		page := console.Console(currentRun, runs, events)
-		err = page.Render(r.Context(), w)
-		if err != nil {
-			log.Errorf("rendering index: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-		}
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", "World")
 	})
 
-	http.HandleFunc("/run/events", func(w http.ResponseWriter, r *http.Request) {
-		var err error
-		run := r.URL.Query().Get("run")
-		stm := r.URL.Query().Get("stm")
-		filter := r.URL.Query().Get("filter")
-		events := []console.LogEvent{}
-
-		params := map[string]interface{}{"run_id": run}
-		sql := "SELECT * FROM events WHERE run_id = :run_id "
-		if !(stm == "" || stm == "all") {
-			sql += " AND event_type = :event_type "
-			params["event_type"] = stm
-		}
-		if filter != "" {
-			sql += " AND event_data LIKE :event_data "
-			params["event_data"] = "%" + filter + "%"
-		}
-		sql += " ORDER BY created_at ASC;"
-
-		res, err := db.NamedQuery(sql, params)
-		if err != nil {
-			log.Errorf("getting event: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		defer res.Close()
-		for res.Next() {
-			var ev console.LogEvent
-			err = res.StructScan(&ev)
-			if err != nil {
-				log.Errorf("scanning event: %v", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			events = append(events, ev)
-		}
-
-		if len(events) == 0 {
-			_, err = w.Write([]byte("<div class=\"text-2xl text-bold\">no events found</div>"))
-			if err != nil {
-				log.Errorf("writing response: %v", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		markup := console.EventList(events)
-		err = markup.Render(r.Context(), w)
-		if err != nil {
-			log.Errorf("rendering index: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-	})
-
-	http.HandleFunc("/restart", func(w http.ResponseWriter, r *http.Request) {
-		log.Info("request: ", r.URL.Path)
-		w.WriteHeader(http.StatusOK)
-	})
-
-	host := ":8080"
-	if p, ok := os.LookupEnv("PORT"); ok {
-		host = ":" + p
-	}
-	log.Infof("listening on %s", host)
-	log.Fatal(http.ListenAndServe(host, nil))
+	e.Logger.Fatal(e.Start(":8080"))
 }
--- go.mod
+++ go.mod
@@ -1,17 +1,27 @@
 module gohtmx
 
-go 1.20
+go 1.21.6
+
+toolchain go1.22.0
 
 require (
 	github.com/a-h/templ v0.2.408
-	github.com/jmoiron/sqlx v1.3.5
-	github.com/mattn/go-sqlite3 v1.14.17
-	github.com/sirupsen/logrus v1.9.0
+	github.com/jdudmesh/gomon-client v0.3.0
+	github.com/labstack/echo/v4 v4.11.4
+	github.com/labstack/gommon v0.4.2
 )
 
 require (
-	github.com/stretchr/testify v1.8.1 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jdudmesh/gomon-ipc v0.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
-
-replace github.com/jdudmesh/gomon-client => ../gomon-client
